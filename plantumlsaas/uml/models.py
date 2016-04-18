283c94cf795e32db0e82c5a0279f4a0e8ecc277a
杉山治 <sugiyama@tars.local>
2016-04-18 20:38:28 +0900

    corresponding to png output

--- plantumlsaas/uml/models.py
+++ plantumlsaas/uml/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class UML(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    query = models.TextField('query')
+    image = models.FileField('image')
+    image_url = models.URLField('image_url')
--- plantumlsaas/uml/views.py
+++ plantumlsaas/uml/views.py
@@ -20,12 +20,26 @@ from django.shortcuts import get_object_or_404
 # from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+from models import UML
+from plantumlsaas.settings import STATIC_URL
+from plantumlsaas.settings import STATIC_ROOT
+
+
 @csrf_exempt
 def get(request):
     try:
         if request.method != "GET":
             raise Http404
         query = request.META['QUERY_STRING']
+
+        uml = UML.objects.get_or_create(query=query)
+        uml.save()
+
+        uml.image = os.path.join(STATIC_ROOT, uml.uuid + ".png")
+        query_string = "@startuml {%s}" % uml.image + os.linesep
+        query_string += query
+        query_string *= "@enduml"
+
         cmd = 'java -jar /usr/local/lib/plantuml.jar'
         p = subprocess.Popen(
             cmd,
@@ -35,17 +49,16 @@ def get(request):
             # stderr=subprocess.PIPE
         )
         p.wait()
-        p.stdin.write(query)
+        p.stdin.write(query_string)
         p.communicate()
-        with open('image.png', "rb") as f:
+
+        with open(uml.image, "rb") as f:
+            uml.image_url = os.path.join(STATIC_URL, uml.uuid + ".png")
+            uml.save()
             return HttpResponse(f.read(), content_type="image/png")
-    except IOError:
-        red = Image.new('RGBA', (1, 1), (255,0,0,0))
-        response = HttpResponse(content_type="image/jpeg")
-        red.save(response, "JPEG")
-        return response
-    except OSError:
-        red = Image.new('RGBA', (1, 1), (255,0,0,0))
-        response = HttpResponse(content_type="image/jpeg")
-        red.save(response, "JPEG")
-        return response
+    except IOError, ioe:
+        return HttpResponse(str(ioe), content_type="text/plain")
+    except OSError, ose:
+        return HttpResponse(str(ose), content_type="text/plain")
+    except Exception, e:
+        return HttpResponse(str(e), content_type="text/plain"   )
